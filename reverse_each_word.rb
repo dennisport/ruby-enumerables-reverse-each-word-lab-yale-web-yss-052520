fc49bd55bafaddd85d3d31229d23207ace233375
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-23 19:38:50 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+  reverse_each_word.each 
+  
+def reverse_each_word(string)
+  reverse_each_word
+  
+  def reverse_each_word(sentence)
+    words_array = sentence.split("")
+    new_array = []
+    new_array = word_array.collect {|word| word.reverse}
+    new_array.join("")
+  end
