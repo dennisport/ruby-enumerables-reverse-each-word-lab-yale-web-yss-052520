b6fe21e509b581b88a5bbb83fa593582b9a8f4f6
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-23 19:53:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@
     words.each do |word|
       reverse_string << "#{word.reverse}"
   end
-  reverse_string
+  reverse_string.chop
 end
   
   def reverse_each_word(string)
@@ -13,6 +13,6 @@ end
     words.collect do |word|
       reverse_string << "#{word.reverse}"
     end
-    reverse_string
+    reverse_string.chop
   end
  
