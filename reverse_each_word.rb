bc958e9a626c0a2b1d5820f67e43317b0ad86328
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-23 19:56:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
     words = string.split
     reverse_string = ""
     words.each do |word|
-      reverse_string << "#{word.reverse} "
+      reverse_string += "#{word.reverse}"
   end
   reverse_string
 end
@@ -11,7 +11,7 @@ end
     words = string.split
     reverse_string = ""
     words.collect do |word|
-      reverse_string << "#{word.reverse} "
+      reverse_string += "#{word.reverse}"
     end
     reverse_string
   end
