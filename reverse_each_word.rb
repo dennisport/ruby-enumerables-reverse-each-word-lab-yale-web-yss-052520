062175c15446f4655791b774c5833756d85f6d87
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-23 19:39:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
-def reverse_each_word(string)
-  reverse_each_word.each 
-  
-def reverse_each_word(string)
-  reverse_each_word
-  
-  def reverse_each_word(sentence)
+ def reverse_each_word(sentence)
     words_array = sentence.split("")
     new_array = []
     new_array = word_array.collect {|word| word.reverse}
